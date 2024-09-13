e9c2415aaa873f19e43b10859470a5bc551e933a
brothergiven <dung3740@gmail.com>
2024-09-13 22:03:36 +0900

    진짜햄버거

--- Week1/minhyeong/Solution.cpp
+++ Week1/minhyeong/Solution.cpp
@@ -2,7 +2,7 @@
 /*
 
 햄버거 찾기
-빵 - 야채 - 고기 - 빵이 햄버거임 
+빵 - 야채 - 고기 - 빵이 햄버거임
 
 배열이 들어왔을 때 빵 - 야채 - 고기 - 빵 순서인 것만 읽어서
 
@@ -13,21 +13,38 @@
 
 
 
-*/ 
+*/
 
-// vector 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// ingredient_len은 배열 ingredient의 길이입니다.
+int solution(int ingredient[], size_t ingredient_len)
+{
+    int answer = 0;
+    int stack[1000000]; // stack의 최대 크기 백만
+    int top = 0;
 
-int solution(vector<int> ingredient){
-    int size = ingredient.size();
+    for (int i = 0; i < ingredient_len; i++)
+    {
+        stack[top++] = ingredient[i]; // push
 
-    int stack[size];
+        // push 한 다음에
+        // 밑에 4개 검사
 
+        if (top >= 4)
+        {
+            if (stack[top - 1] == 1 &&
+                stack[top - 2] == 3 &&
+                stack[top - 3] == 2 &&
+                stack[top - 4] == 1)
+            {
+                answer++;
+                top -= 4;
+            }
+        }
+    }
 
-
-
-
+    return answer;
 }
