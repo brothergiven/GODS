f73878613896df4e25120a579e07bcbde53c88f7
minseok <minsuk6803@gmail.com>
2024-09-25 00:39:07 +0900

    week2 문제 풀이 완료

--- Week1/minseok/Solution.java
+++ Week1/minseok/Solution.java
@@ -17,20 +17,20 @@ class Solution {
                 curMaxMove++;//자투리 시간이 편도로 이동할 수 있는 시간보다 길면 편도로 한 번 더 운송할 수 있음
             long curMaxWeight = curMaxMove * curWeight;//운반할 수 있는 최대 양
 
-            totalGold +=Math.min(curGold,curMaxWeight);//현재 도시에서 운반할 수 있는 금의 양
-            totalSilver +=Math.min(curSilver,curMaxWeight);//현재 도시에서 운반할 수 있는 은의 양
-            totalWeight +=Math.min(curGold+curSilver,curMaxWeight);//현재 도시에서 운반할 수 있는 전체 자원의 양
+            totalGold +=Math.min(curGold,curMaxWeight);//현재 도시에서 최대로 운반할 수 있는 금의 양
+            totalSilver +=Math.min(curSilver,curMaxWeight);//현재 도시에서 최대로 운반할 수 있는 은의 양
+            totalWeight +=Math.min(curGold+curSilver,curMaxWeight);//현재 도시에서 최대로 운반할 수 있는 전체 자원의 양
         }
         return totalGold >= a && totalSilver >= b && totalWeight >= a + b;//모든 도시를 다 확인한 다음에 조건을 충족하는지 확인
 
     }
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
         long start = 1;
-        long end = 2* maxCyCleTime * maxWeight;//걸릴 수 있는 최악의 시간
+        long end = maxCyCleTime * maxWeight;//걸릴 수 있는 최악의 시간
         long answer = end;
         while(start<=end){//이분 탐색으로 반복문 돎
             long mid = start+(end-start)/2;
-            if(checkAvailableMoving(a,b,g,s,w,t,mid)){//조건에 충족하면 정답 수정후 상한선 줄임
+            if(checkAvailableMoving(a,b,g,s,w,t,mid)){// 조건에 충족하면 정답 수정후 상한선 줄임
                 answer = mid;
                 end=mid-1;
                 continue;
